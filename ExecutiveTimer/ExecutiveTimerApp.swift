8e97d5581fa4b0fac01101b2d98b2186b0fc7dbf
LutLastic Fantastic <50748557+LutLas@users.noreply.github.com>
2022-10-14 20:23:05 +0200

    Initial Commit

---
+++ ExecutiveTimer/ExecutiveTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExecutiveTimerApp.swift
+//  ExecutiveTimer
+//
+//  Created by Lutaka Muyoba Chihota on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ExecutiveTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
